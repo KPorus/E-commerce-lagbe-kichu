cf32d652238991c0c345604dcb0a4fe854ce3a22
KPorus <designerfardin2021@gmail.com>
2025-04-14 15:45:52 +0600

    working with issues

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  // app.enableCors();
+  app.enableCors({
+    origin: 'https://e-commerce-lagbe-kichu.vercel.app/',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
