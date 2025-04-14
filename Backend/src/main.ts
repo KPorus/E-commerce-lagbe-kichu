7f46717b7c7426adc1299721c6c9921bcc2477c5
KPorus <designerfardin2021@gmail.com>
2025-04-14 14:54:54 +0600

    some update

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,8 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'https://e-commerce-lagbe-kichu.vercel.app/',
+    origin: 'https://e-commerce-lagbe-kichu.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   app.useGlobalPipes(
